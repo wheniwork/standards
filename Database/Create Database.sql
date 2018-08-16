764e31277bf8031ed69d04fc3f8e209bfa7c5ad8
Elliot Courant <ecourant@live.com>
2018-08-15 22:10:36 -0500

    Created view for summarizing weeks of hours worked.
    I need to tweak this though. I did add a cutoff so that if a shift were to go beyond the end of that week it would not count the extra hours. But i need to now figure out how to carry those hours onto the following week.

--- Database/Create Database.sql
+++ Database/Create Database.sql
@@ -44,8 +44,10 @@ CREATE TABLE public.shifts (
 INSERT INTO public.shifts (manager_id, employee_id, start_time, end_time)
 VALUES (3, 1, TIMEZONE('CDT', '2018-08-11 8:00AM'), TIMEZONE('CDT', '2018-08-11 2:00PM')),
        (3, 1, TIMEZONE('CDT', NOW()), TIMEZONE('CDT', NOW()) + INTERVAL '2 Hour'),
+       (3, 1, TIMEZONE('CDT', NOW()) + INTERVAL '1 Day', TIMEZONE('CDT', NOW()) + INTERVAL '6 Day 3 Hour 11 minutes'),
        (3, 2, TIMEZONE('CDT', NOW()) - INTERVAL '1 Hour', TIMEZONE('CDT', NOW()) + INTERVAL '1 Hour'),
        (3, 3, TIMEZONE('CDT', NOW()) + INTERVAL '1 Hour', TIMEZONE('CDT', NOW()) + INTERVAL '3 Hour');
+UPDATE public.shifts SET break = 0.5;
 
 
 DROP VIEW IF EXISTS public.vw_users_api;
@@ -130,12 +132,55 @@ CREATE VIEW public.vw_shifts_detailed_api AS
 
 DROP VIEW IF EXISTS public.vw_shifts_summary_api;
 CREATE VIEW public.vw_shifts_summary_api AS
-  SELECT id,
-         manager_id,
-         employee_id,
-         break,
-         to_char(start_time, 'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY') AS start_time,
-         to_char(end_time, 'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY')   AS end_time,
-         to_char(created_at, 'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY') AS created_at,
-         to_char(updated_at, 'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY') AS updated_at
-  FROM public.shifts;
+  SELECT a.employee_id,
+         to_json(row (employee.id,
+                     employee.name,
+                     employee.email,
+                     employee.phone,
+                     employee.role,
+                     to_char(employee.created_at, 'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY'),
+                     to_char(employee.updated_at, 'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY')) :: public.user) AS employee_user,
+         a.week,
+         a.week_start,
+         a.week_end,
+         COUNT(distinct a.shift_id)                                                              AS total_shifts,
+         round(CAST(EXTRACT(epoch FROM SUM(a.hours)) / 3600 as numeric), 2)                      AS total_time,
+         to_char(date_part('epoch', SUM(a.hours)) * INTERVAL '1 second',
+                 'FMHH24 Hour(s) FMMI ') || 'Minute(s)'                                          AS total_time_formatted,
+         round(CAST(EXTRACT(epoch FROM SUM(a.breaks) * INTERVAL '1 Hour') / 3600 as numeric), 2) AS total_break_time,
+         to_char(date_part('epoch', SUM(a.breaks) * INTERVAL '1 Hour') * INTERVAL '1 second',
+                 'FMHH24 Hour(s) FMMI ') ||
+         'Minute(s)'                                                                             AS total_break_time_formatted
+  FROM (SELECT employee_id,
+               to_char(start_time, 'YYYYWW')                 AS week,
+
+               to_char(to_date(to_char(start_time, 'YYYYWW'), 'YYYYWW'),
+                       'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY') AS week_start,
+
+               to_char(to_date(to_char(start_time, 'YYYYWW'), 'YYYYWW') + INTERVAL '6.99999999999 Days',
+                       'Dy, Mon DD HH24:MI:SS.MS OF00 YYYY') AS week_end,
+
+               id                                            AS shift_id,
+            -- We are taking the end date of the current week or the end date of the shift, whichever is less we will use as the end date for this shift.
+            -- This is to fix a problem where there could be more total_time in a week than there are actually hours in a week.
+            -- This happens when a shift extends beyond the week, such as a late shift that starts on a sunday night and finishes monday morning.
+            -- That shifts hours would be logged on two seperate weeks. If i can get this working.
+            -- Getting hours to cut off at the end of the week is one thing, but then adding hours to the next week is tricker.
+               ((LEAST(to_date(to_char(start_time, 'YYYYWW'), 'YYYYWW') + INTERVAL '6.99999999999 Days', end_time) -
+                 start_time) - (break * INTERVAL '1 Hour'))  AS hours,
+
+               break                                         AS breaks
+        FROM public.shifts
+        WHERE employee_id IS NOT NULL) a
+         INNER JOIN public.users employee ON employee.id=a.employee_id
+  GROUP BY a.employee_id,
+           a.week,
+           a.week_start,
+           a.week_end,
+           employee.id,
+           employee.name,
+           employee.email,
+           employee.phone,
+           employee.role,
+           employee.created_at,
+           employee.updated_at;
